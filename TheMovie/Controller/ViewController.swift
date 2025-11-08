b7703e8824c4143a2cab9d76c170a71bf006ad42
Kirito <mghtetpai.1999@gmail.com>
2025-11-08 20:40:40 +0700

    Initial Commit. Setup files and folders. Add colors, icons and pod files

---
+++ TheMovie/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovie
+//
+//  Created by Kiyotaka Kirito on 08/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
