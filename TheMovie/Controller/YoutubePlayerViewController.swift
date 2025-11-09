03b522ecc73f6ceef53172862030b30a0348371b
Kirito <mghtetpai.1999@gmail.com>
2025-11-09 15:05:48 +0700

    Completed YoutubePlayer UI

---
+++ TheMovie/Controller/YoutubePlayerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  YoutubePlayerViewController.swift
+//  TheMovie
+//
+//  Created by Kiyotaka Kirito on 09/11/2025.
+//
+
+import UIKit
+
+class YoutubePlayerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
