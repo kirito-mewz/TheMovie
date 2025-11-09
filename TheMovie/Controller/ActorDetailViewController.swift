42d215a37f21628a49cc6075ee7e61e25700ba53
Kirito <mghtetpai.1999@gmail.com>
2025-11-09 13:34:57 +0700

    Completed ActorDetail UI

---
+++ TheMovie/Controller/ActorDetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ActorDetailViewController.swift
+//  TheMovie
+//
+//  Created by Kiyotaka Kirito on 09/11/2025.
+//
+
+import UIKit
+
+class ActorDetailViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var actorImageView: UIImageView!
+    @IBOutlet weak var roleLabel: UILabel!
+    @IBOutlet weak var actorNameLabel: UILabel!
+    @IBOutlet weak var popularityLabel: UILabel!
+    
+    @IBOutlet weak var biographyLabel: UILabel!
+    
+    @IBOutlet weak var detailNamelLabel: UILabel!
+    @IBOutlet weak var detailAkaLabel: UILabel!
+    @IBOutlet weak var detailBirthdayLabel: UILabel!
+    @IBOutlet weak var detailPobLabel: UILabel!
+    @IBOutlet weak var detailRoleLabel: UILabel!
+    
+    @IBOutlet weak var relatedMovieCollectionView: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension ActorDetailViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueCell(ofType: ActorCollectionViewCell.self, for: indexPath, shouldRegister: true)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: collectionView.frame.width / 2.5, height: collectionView.frame.height)
+    }
+    
+}
